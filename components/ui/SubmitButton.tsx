c648fa6fcbb1ad86118860f42ea26c98183a4edc
Aurora Scharff <aurora.sofie@gmail.com>
2024-09-19 17:27:05 +0200

    Implemnt submitbutton

--- components/ui/SubmitButton.tsx
+++ components/ui/SubmitButton.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import React from 'react';
+import { useFormStatus } from 'react-dom';
 import Button from './Button';
 import { SpinnerIcon } from './icons';
 import type { button } from './Button';
@@ -18,9 +21,12 @@ export default function SubmitButton({
   theme,
   ...otherProps
 }: Props & React.HTMLProps<HTMLButtonElement> & VariantProps<typeof button>) {
+  const { pending } = useFormStatus();
+  const isSubmitting = pending || loading;
+
   return (
-    <Button theme={theme} {...otherProps} disabled={loading || disabled} type="submit" className={className}>
-      {loading ? (
+    <Button theme={theme} {...otherProps} disabled={isSubmitting || disabled} type="submit" className={className}>
+      {isSubmitting ? (
         <div className="flex items-center justify-center gap-2">
           {children}
           <div className="h-fit w-fit animate-spin">
