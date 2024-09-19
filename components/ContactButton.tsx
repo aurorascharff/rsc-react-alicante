8ffc6a4a69ce61f34aae4ff62b4786bbc8637d16
Aurora Scharff <aurora.sofie@gmail.com>
2024-09-19 16:35:09 +0200

    Update

--- components/ContactButton.tsx
+++ components/ContactButton.tsx
@@ -1,6 +1,8 @@
+'use client';
+
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import React from 'react';
+import { usePathname, useRouter } from 'next/navigation';
+import React, { useTransition } from 'react';
 import { cn } from '@/utils/cn';
 import { routes, useSafeSearchParams } from '@/validations/routeSchema';
 import type { Contact } from '@prisma/client';
@@ -9,13 +11,22 @@ export default function ContactButton({ contact }: { contact: Contact }) {
   const pathName = usePathname();
   const isActive = pathName.includes(routes.contactId({ contactId: contact.id }));
   const { q } = useSafeSearchParams('home');
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
 
   return (
     <Link
       className={cn(
+        isPending ? 'pending' : '',
         isActive ? 'bg-primary text-white' : 'hover:bg-gray',
         'flex w-full items-center justify-between gap-4 overflow-hidden whitespace-pre rounded-lg p-2 hover:no-underline',
       )}
+      onClick={e => {
+        e.preventDefault();
+        startTransition(() => {
+          router.push(routes.contactId({ contactId: contact.id, search: { q } }));
+        });
+      }}
       href={routes.contactId({ contactId: contact.id, search: { q } })}
     >
       {contact.first || contact.last ? (
--- data/services/getContact.ts
+++ data/services/getContact.ts
@@ -1,7 +1,10 @@
 import { notFound } from 'next/navigation';
 import { prisma } from '@/db';
+import { slow } from '@/utils/slow';
 
 export async function getContact(contactId: string) {
+  await slow();
+
   const contact = await prisma.contact.findUnique({
     where: {
       id: contactId,
